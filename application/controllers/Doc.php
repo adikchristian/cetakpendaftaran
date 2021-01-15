0ba27dfc119f6ef3d7974299bf9f2cab67fb5663
adikchristian <adichristian610@gmail.com>
2021-01-15 20:23:36 +0800

    fix export excel pendaftaran

--- application/controllers/Doc.php
+++ application/controllers/Doc.php
@@ -1,6 +1,11 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+//require('./application/third_party/phpoffice/vendor/autoload.php');
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
 class Doc extends CI_Controller
 {
 
@@ -47,5 +52,51 @@ class Doc extends CI_Controller
 			'dt_bon' => $this->Query->getAmbiAndKembali($id)
 		];
 		$this->load->view('laporan_bon_print', $data);
+	}
+
+	public function expPendaftaran($tgl1,$tgl2,$poli,$norm)
+	{
+		$sql = $this->Query->getLapPendaftaran($tgl1,$tgl2,$poli,$norm);
+
+		$spreadsheet = new Spreadsheet;
+
+        $spreadsheet->setActiveSheetIndex(0)
+                      ->setCellValue('A1', 'No')
+					  ->setCellValue('B1', 'No RM')
+					  ->setCellValue('C1', 'No REG')
+                      ->setCellValue('D1', 'Nama Pasien')
+                      ->setCellValue('E1', 'Tanggal Kontrol')
+					  ->setCellValue('F1', 'Polilinik')
+					  ->setCellValue('G1', 'Jam')
+					  ->setCellValue('H1', 'Dokter')
+					  ->setCellValue('I1', 'Keluhan');
+
+        $kolom = 2;
+        $nomor = 1;
+        foreach($sql as $row) {
+
+            $spreadsheet->setActiveSheetIndex(0)
+                           ->setCellValue('A' . $kolom, $nomor)
+                           ->setCellValue('B' . $kolom, $row->no_rm)
+                           ->setCellValue('C' . $kolom, $row->no_reg)
+                           ->setCellValue('D' . $kolom, $row->nama_pasien)
+						   ->setCellValue('E' . $kolom, $row->tgl_kontrol)
+						   ->setCellValue('F' . $kolom, $row->name_poli)
+						   ->setCellValue('G' . $kolom, $row->jam)
+						   ->setCellValue('H' . $kolom, $row->dokter_name)
+						   ->setCellValue('I' . $kolom, $row->keluhan);
+
+            $kolom++;
+            $nomor++;
+
+        }
+
+        $writer = new Xlsx($spreadsheet);
+
+        header('Content-Type: application/vnd.ms-excel');
+	  	header('Content-Disposition: attachment;filename="Laporan-Pendaftaran.xlsx"');
+	    header('Cache-Control: max-age=0');
+
+	    $writer->save('php://output');
 	}
 }
--- application/models/Query.php
+++ application/models/Query.php
@@ -18,5 +18,30 @@ class Query extends CI_Model{
         $query = $this->db->query("SELECT * FROM t_pengembalian WHERE id_pengambilan='$id'");
         return $query->row();
     }
+
+    public function getLapPendaftaran($tgl1,$tgl2,$poli=null,$norm=null)
+    {
+        if($poli==null or $poli=="none"){
+            $wherePoli = "";
+        }else{
+            $wherePoli = "AND t_poli.id_poli='$poli'";
+        }
+
+        if($norm==null or $poli=="none"){
+            $wherePasien="";
+        }else{
+            $wherePasien="AND t_daftar.no_rm='$norm'";
+        }
+
+        return $this->db->query("SELECT `t_pasien`.`no_rm`, `t_pasien`.`nama_pasien`, `t_daftar`.`no_reg`, `t_daftar`.`tgl_kontrol`, `t_poli`.`name_poli`, `t_daftar`.`keluhan`, `t_dokter`.`dokter_name`, `t_jadwal`.`jam` 
+        FROM `t_daftar` 
+        LEFT OUTER JOIN t_pasien ON(t_pasien.no_rm=t_daftar.no_rm)
+        LEFT OUTER JOIN t_jadwal ON(t_jadwal.id_jadwal=t_daftar.id_jadwal)
+        LEFT OUTER JOIN t_dokter ON(t_dokter.id_dokter=t_jadwal.id_dokter)
+        LEFT OUTER JOIN t_poli ON(t_poli.id_poli=t_dokter.id_poli)
+        WHERE `t_daftar`.`tgl_kontrol` BETWEEN '$tgl1' AND '$tgl2' 
+        AND `t_daftar`.`visible` = '1' 
+        $wherePoli $wherePasien ORDER BY t_daftar.tgl_kontrol ASC")->Result();
+    }
 }
 ?>
